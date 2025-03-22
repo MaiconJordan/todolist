83e8e172afd09d02a6577810dd0e78e7a8e33411
Maicon <maiconplus@gmail.com>
2025-03-22 12:30:22 -0300

    metodos CRUD criado

---
+++ src/main/java/br/com/maicon/todolist/repository/TodoRepository.java
@@ -0,0 +1,11 @@
+package br.com.maicon.todolist.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.com.maicon.todolist.entity.Todo;
+
+public interface TodoRepository extends JpaRepository<Todo, Long>{
+
+    
+
+}
